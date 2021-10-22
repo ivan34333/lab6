3ea268c452e4e80bca0d5cfb30156d58df6f3c33
ivan <trunovivan641@gmail.com>
2021-10-23 00:09:33 +0300

    Not first try

---
+++ lr6 cod.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	int x,a;
+	x > 999;
+	cout << "Input number" << endl;
+	cin >> x;
+	a = x / 1000;
+	(a > 10) || (a < 10) ? (a % 10) : (a / 1);
+	cout << a;
+	
+
+}
